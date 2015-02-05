21046431bf4fa6715122c0d93a8ca231eefb0834
halley73 <Universo73>
2015-02-05 12:03:16 +0100

    Assertion - Expect - Should

--- test/main.js
+++ test/main.js
@@ -1,7 +1,25 @@
-var assert = require('assert');
+var assert = require('assert'),
+    chai = require('chai'),
+    expect = chai.expect;
+
+chai.should();
+
+function fixedValue () {
+    return 10;
+}
 
 describe('Come utente voglio vedere i miei dati', function () {
-   it('should give some data to the user', function () {
+    it('should give some data to the user', function () {
         assert.equal(process.env.NODE_ENV, 'testing');
-   });
+        expect(process.env.NODE_ENV).to.equal('testing');
+        process.env.NODE_ENV.should.equal('testing');
+    });
+
+    it('should return a fixed value', function () {
+        var result = fixedValue();
+
+        result.should.be.not.null;
+        result.should.be.a('number');
+        result.should.equal(10);
+    });
 });
